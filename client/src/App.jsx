085dc06d572785c584ea0541c6f4a7f1eea1bbda
WipaBG <aleksandar_niagolov@abv.bg>
2024-08-04 02:01:04 +0300

    X-Authorization access token

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,10 +25,12 @@ function App() {
   const [authState, setAuthState] = useState({});
 
   const changeAuthState = (state)=>{
+    localStorage.setItem('accessToken', state.accessToken)
     setAuthState(state);
   }
 
   const contextData = {
+    userId: authState._id,
     email: authState.email,
     accessToken: authState.accessToken,
     isAuthenticated: !!authState.email,
