8e439db2a3f37571bc295238e28d7639ef79edae
WipaBG <aleksandar_niagolov@abv.bg>
2024-08-03 23:51:01 +0300

    Remove saved password in AuthData

--- client/src/components/Header/Navigation.jsx
+++ client/src/components/Header/Navigation.jsx
@@ -1,20 +1,31 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext'
 
 export default function Navigation() {
+    const { isAuthenticated } = useContext(AuthContext);
+
     return (
         <nav>
             <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/rooms">Rooms</Link></li>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/register">Register</Link></li>
-                    <li><Link to="/logout">Logout</Link></li>
-                    <li><Link to ='/create-reservation'>Reserve</Link></li>
-                    <li><Link to ='/reservations'>Reservations</Link></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                {isAuthenticated ? (
+                    <>
+                        <li><Link to="/rooms">Rooms</Link></li>
+                        <li><Link to="/logout">Logout</Link></li>
+                        <li><Link to='/create-reservation'>Reserve</Link></li>
+                        <li><Link to='/reservations'>Reservations</Link></li>
 
 
+                    </>
+                ) : (
+                    <>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/register">Register</Link></li>
 
+                    </>
+                )}
 
             </ul>
         </nav>
